ce2c6a718d23b26f383fbe5f448476fe8f0d0c81
Helen Yanushevskaya <qahelping@gmail.com>
2024-04-18 19:59:22 +0300

    add main file

---
+++ main2.py
@@ -0,0 +1,19 @@
+import math
+import pytest
+
+def main_func(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15,
+                   arg16, arg17):
+    sum1 = arg1 + arg2
+
+    sum2 = arg3 + arg4
+    return math.pi
+
+
+def funcasdfgbhnjm2(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15,
+                   arg16, arg17):
+    pass
+
+
+def funcasdfgbhnjm3(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15,
+                   arg16, arg17):
+    pass
